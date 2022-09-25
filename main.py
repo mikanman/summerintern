bd775bb0f04f924f37bd8e0564287cfc7cc095f1
mikanman <kosp200@gmail.com>
2022-09-26 01:05:25 +0900

    切り取った画像を用意する前

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import cv2
 import mediapipe as mp
 import matplotlib.pyplot as plt
+import numpy as np
 
 import tensorflow as tf
 
 import cut
 
 cap = cv2.VideoCapture(0)
+
+# 初期設定
 hands = mp.solutions.hands.Hands(
     static_image_mode=True,
     max_num_hands=1,
     min_detection_confidence=0.5
     )
 
-# new_model = tf.keras.models.load_model('saved_model')
+label=np.array(['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん'])
 
+model = tf.keras.models.load_model('model/my_model.h5')
 
 while True:
 
   ret, image = cap.read()
 
-  results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+  img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  img_size = cv2.resize(img, dsize=(256, 256))/ 255.0
+  img_expand = img_size[np.newaxis, ...]
+
+  predictions_single = model.predict(img_expand)
+  predictions_single = predictions_single[0]
+  answer = np.argpartition(-predictions_single, 5)[:5]
+
+  results = hands.process(img)
   
   image_height, image_width, _ = image.shape
 
@@ -76,6 +88,15 @@ while True:
     (0,0,255),
     3
   )
+  print(label[answer])
+  # cv2.putText(image,
+  #             text='sample text',
+  #             org=(100, 300),
+  #             fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+  #             fontScale=1.0,
+  #             color=(0, 255, 0),
+  #             thickness=2,
+  #             lineType=cv2.LINE_4)
 
   cv2.imshow("Frame", image)
 
--- test.py
+++ test.py
@@ -1,4 +1,30 @@
 import tensorflow as tf
+import numpy as np
+import cv2
+
+
+label=np.array(['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん'])
+
 
 
 new_model = tf.keras.models.load_model('model/my_model.h5')
+
+
+img = cv2.cvtColor(cv2.imread('IMG_1823.jpg'), cv2.COLOR_BGR2RGB)
+
+img = cv2.resize(img, dsize=(256, 256))/ 255.0
+
+
+img_expand = img[np.newaxis, ...]
+
+
+
+predictions_single = new_model.predict(img_expand)
+
+predictions_single = predictions_single[0]
+
+a = np.argpartition(-predictions_single, 5)[:5]
+
+print(label[a])
+
+print(img_expand.shape)
